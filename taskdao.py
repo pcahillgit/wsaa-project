1780c9325fc0e2773850a99c630e33eda42c7499
Paul Cahill <paulcahill97@outlook.com>
2025-05-07 17:58:59 +0100

    added CRUD routes to Flask API

--- taskdao.py
+++ taskdao.py
@@ -23,4 +23,41 @@ class TaskDAO:
         db.close()
         return results
 
+    def findByID(self, id):
+        db = self.getConnection()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM tasks WHERE id = %s", (id,))
+        result = cursor.fetchone()
+        db.close()
+        return result
+
+    def create(self, task):
+        db = self.getConnection()
+        cursor = db.cursor()
+        sql = "INSERT INTO tasks (title, assigned_to, done) VALUES (%s, %s, %s)"
+        values = (task['title'], task['assigned_to'], task['done'])
+        cursor.execute(sql, values)
+        db.commit()
+        new_id = cursor.lastrowid
+        db.close()
+        return new_id
+
+    def update(self, id, task):
+        db = self.getConnection()
+        cursor = db.cursor()
+        sql = "UPDATE tasks SET title = %s, assigned_to = %s, done = %s WHERE id = %s"
+        values = (task['title'], task['assigned_to'], task['done'], id)
+        cursor.execute(sql, values)
+        db.commit()
+        db.close()
+        return True
+
+    def delete(self, id):
+        db = self.getConnection()
+        cursor = db.cursor()
+        cursor.execute("DELETE FROM tasks WHERE id = %s", (id,))
+        db.commit()
+        db.close()
+        return True
+
 taskDAO = TaskDAO()
